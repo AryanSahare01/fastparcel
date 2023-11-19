0c1b36f3b9efb9ce39a054815d1ee5a9ee6d2d74
Aryan Sahare <ureviews4@gmail.com>
2023-11-20 00:20:03 +0530

    Addition to project

--- core/views.py
+++ core/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-def home(request):
+from . import forms
+
+
+def home(request):  
     return render(request, 'home.html')
 
+@login_required()
 def customer_page(request):
     return render(request, 'customer.html')
 
+@login_required()
 def courier_page(request):
     return render(request, 'courier.html')
+
+def sign_up(request):
+    form = forms.SignUpForm()
+    return render(request, 'sign_up.html', {'form': form})
 
 
 
