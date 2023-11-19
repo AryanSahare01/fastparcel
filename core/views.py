a5cbe98a02d53b7685c5fa429be5aeced89d6688
Aryan Sahare <ureviews4@gmail.com>
2023-11-20 00:45:55 +0530

    commit

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from . import forms
@@ -17,6 +18,20 @@ def courier_page(request):
 
 def sign_up(request):
     form = forms.SignUpForm()
+
+    if request.method == "POST":
+        form = forms.SignUpForm(request.POST)
+        
+        if form.is_valid():
+            email = form.cleaned_data.get('email').lower()
+            
+            user = form.save(commit=False)
+            user.username = email
+            user.save()
+
+            login(request, user)
+            return redirect('/')
+
     return render(request, 'sign_up.html', {'form': form})
 
 
